595550cedcdd768d704977201cf998355c4e7110
jun4869 <jun2194k@gmail.com>
2021-09-27 22:24:47 +0900

    新規追加の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,23 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+  
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: 'Success!'
+    else
+      flash[:alert] = "Save error!"
+      render :new
+    end
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:title)
+    end
 end
